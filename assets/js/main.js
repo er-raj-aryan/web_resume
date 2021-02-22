e7c3da55533bb8fadc8536de3697ff87912df1bb
No1RajAryan <55110091+No1RajAryan@users.noreply.github.com>
2021-02-22 21:31:47 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,4 +119,5 @@ resumeButton.addEventListener("click",()=> {
     generateResume()
 
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    setTimeout(removeScale, 5000)
 })
